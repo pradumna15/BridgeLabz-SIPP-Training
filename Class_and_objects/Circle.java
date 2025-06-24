e559cbaaf4ef859ee1f2587ed27eb624afdbf76b
pradumna15 <pradumna.sharma15@gmail.com>
2025-06-24 12:42:31 +0530

    [Pradumna sharma].Add practice problems for day9

---
+++ Class_and_objects/Circle.java
@@ -0,0 +1,29 @@
+package Class_and_objects;
+
+public class Circle {
+    double radius;
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+
+    public double calcArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calcCircumference(){
+        return 2 * Math.PI * radius;
+    }
+
+    public void DisplayDetails(){
+        System.out.println("Radius:" + radius);
+        System.out.println("Area"+calcArea());
+        System.out.println("Circumference"+calcCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+
+        circle.DisplayDetails();
+    }
+}
