3923c34a878f26365a642e6260b9c5254f37b20b
pradumna15 <pradumna.sharma15@gmail.com>
2025-09-09 23:52:45 +0530

    delete Functional interface

--- Functional_interfaces/BackgroundJobExecution.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class BackgroundJobExecution {
-    public static void main(String[] args) {
-        Runnable job = () -> System.out.println("Job executed");
-        Thread t = new Thread(job);
-        t.start();
-    }
-}
